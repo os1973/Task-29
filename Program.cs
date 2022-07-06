eaa160b56ca2928e91ea79466680cea4f1a8679f
Oksana <nemoria@mail.ru>
2022-07-06 14:08:59 +0700

    Add code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] array = new int [8];
+//int number = Next(1, 2, 5, 7, 19, 1, 2, 5);
+for(int i = 1, i < array.Length;);
+{
+    array[i] = (1, 2, 5, 7, 19, 1, 2, 5);
+}
+for(int i = 1, i < array.Length);
+{
+    Console.Write($"{array[i]}");
+}
+//Console.WriteLine($"{array}");
